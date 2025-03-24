4889b3a43b3f74239d296101a7bb552689b26dd9
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-03-24 21:48:53 +0530

    Add Project Files

---
+++ Social-Login/src/main/java/com/spring/Social_Login/SocialLoginApplication.java
@@ -0,0 +1,13 @@
+package com.spring.Social_Login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialLoginApplication.class, args);
+	}
+
+}
