24d596e748ef0eecf4703324df41fd1b2def0b13
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-03-26 21:41:35 +0530

    Security Config Files Create

--- SpringSecurity/src/main/java/com/spring/SpringSecurity/controller/Hello.java
+++ SpringSecurity/src/main/java/com/spring/SpringSecurity/controller/Hello.java
@@ -1,12 +1,13 @@
 package com.spring.SpringSecurity.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Hello {
     @GetMapping("/")
-    public String hello() {
-        return "Hello World";
+    public String hello(HttpServletRequest request) {
+        return "Hello World  "+request.getSession().getId();
     }
 }
