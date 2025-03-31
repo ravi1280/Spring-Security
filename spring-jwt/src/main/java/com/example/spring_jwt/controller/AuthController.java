01a144e17d0cbe6beaae8a69cb8b85e79bd62c16
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-03-31 23:29:22 +0530

    Update Auth Controller

--- spring-jwt/src/main/java/com/example/spring_jwt/controller/AuthController.java
+++ spring-jwt/src/main/java/com/example/spring_jwt/controller/AuthController.java
@@ -2,9 +2,8 @@ package com.example.spring_jwt.controller;
 
 import com.example.spring_jwt.entity.User;
 import com.example.spring_jwt.service.AuthService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -13,13 +12,21 @@ import java.util.List;
 public class AuthController {
     private final AuthService authService;
 
+
     public AuthController(AuthService authService) {
         this.authService = authService;
+
     }
 
     @GetMapping()
     public List<User> getUser() {
         return authService.getAllUser();
+    }
+
+    @PostMapping("/createuser")
+    public User createUser(@RequestBody User user) {
+        System.out.println(user.getPassword().toString());
+        return authService.createUser(user);
 
     }
 }
--- spring-jwt/src/main/java/com/example/spring_jwt/service/AuthService.java
+++ spring-jwt/src/main/java/com/example/spring_jwt/service/AuthService.java
@@ -2,6 +2,7 @@ package com.example.spring_jwt.service;
 
 import com.example.spring_jwt.entity.User;
 import com.example.spring_jwt.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,13 +10,23 @@ import java.util.List;
 @Service
 public class AuthService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public AuthService(UserRepository userRepository) {
+    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
 
     public List<User> getAllUser() {
+
         return userRepository.findAll();
+    }
+
+    public User createUser(User user) {
+//        System.out.println(user.getPassword().toString());
+        User newUser = new User(user.getId(), user.getName(), user.getEmail(), user.getUsername(), passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(newUser);
+
     }
 }
